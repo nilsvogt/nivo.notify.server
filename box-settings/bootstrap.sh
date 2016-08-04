02b5f587a740b036528ded5466054112f2992776
Nils Vogt <hello.nils.vogt@googlemail.com>
2016-08-04 09:48:55 +0200

    added a nginxrestart as a result of a successfully php7fpm restart

--- box-settings/bootstrap.sh
+++ box-settings/bootstrap.sh
@@ -6,22 +6,23 @@ document_root="/vagrant/app/public"
 
 # add repository ppa:ondrej/php in order to install the php7 stack
 #
+sudo apt-get update && 
 sudo apt-get install -y language-pack-en-base &&
 sudo LC_ALL=en_US.UTF-8 add-apt-repository ppa:ondrej/php &&
-sudo add-apt-repository ppa:ondrej/php &&
+sudo add-apt-repository ppa:ondrej/php
 
 # Once the PPA is installed, update the local package cache to include its contents:
 #
-apt-get update
+apt-get update &&
 
 # install server (nginx)
 #
-sudo apt-get install -y nginx
+sudo apt-get install -y nginx &&
 
 # set nginx config
 #
-sudo rm /etc/nginx/sites-available/default
-sudo cp /vagrant/box-settings/nginx.default /etc/nginx/sites-available/default
+sudo rm /etc/nginx/sites-available/default &&
+sudo cp /vagrant/box-settings/nginx.default /etc/nginx/sites-available/default 
 
 # link document root
 #
@@ -32,32 +33,32 @@ fi
 
 # install php 7 stack
 #
-sudo apt-get install -y php7.0-fpm php7.0 php7.0-mysql php7.0-curl php7.0-gd php7.0-intl php-pear php7.0-imap php7.0-mcrypt php7.0-sqlite3 snmp
+sudo apt-get install -y php7.0-fpm php7.0 php7.0-mysql php7.0-curl php7.0-gd php7.0-intl php-pear php7.0-imap php7.0-mcrypt php7.0-sqlite3 snmp php7.0-mbstring &&
 
 # install xdebug
 #
 sudo apt-get install -y php7.0-dev &&
 sudo pecl install xdebug &&
-sudo bash -c "cat /vagrant/box-settings/xdebug-php.ini >> /etc/php/7.0/fpm/php.ini"
+sudo bash -c "cat /vagrant/box-settings/xdebug-php.ini >> /etc/php/7.0/fpm/php.ini" &&
 
 # install mysql-server (without prompt)
 #
 sudo debconf-set-selections <<< 'mysql-server mysql-server/root_password password root' &&
 sudo debconf-set-selections <<< 'mysql-server mysql-server/root_password_again password root' &&
-sudo apt-get -y install mysql-server
+sudo apt-get -y install mysql-server &&
 
 # install mysql-client
 #
-sudo apt-get install -y mysql-client
+sudo apt-get install -y mysql-client &&
 
 # install git
 #
-sudo apt-get install -y git
-
+sudo apt-get install -y git &&
+ 
 # install composer
 #
 curl -Ss https://getcomposer.org/installer | php &&
-mv composer.phar /usr/local/bin/composer
+mv composer.phar /usr/local/bin/composer 
 
 # bootstrap the public root
 #
@@ -70,5 +71,5 @@ fi
 
 # restart server
 #
-sudo service php7.0-fpm restart
+sudo service php7.0-fpm restart &&
 sudo service nginx restart
